0bb68d8ac433962b52c59b6ff9662cabe627bf9d
Thierry Graff <thierry@conjecto.com>
2023-01-18 17:42:19 +0100

    #11 - restore process complet pour test en beta

--- manage/dbmigrate/2023-01-16-fix-parcelle.go
+++ manage/dbmigrate/2023-01-16-fix-parcelle.go
@@ -18,34 +18,12 @@ import (
 )
 
 func Migrate_2023_01_16_fix_parcelle(ctx *ctxt.Context) {
-	// versionSCTL := "2023-01-11"
-	// install.CreateTable(ctx, "parcelle")
-	// install.AddParcelleCode11(ctx, versionSCTL)
-	// install.FillParcelle(ctx, versionSCTL)
-	// install.FillLiensParcelleLieudit(ctx, versionSCTL)
-	// install.FillLiensParcelleFermier(ctx, versionSCTL)
+	versionSCTL := "2023-01-11"
+	install.CreateTable(ctx, "parcelle")
+	install.AddParcelleCode11(ctx, versionSCTL)
+	install.FillParcelle(ctx, versionSCTL)
+	install.FillLiensParcelleLieudit(ctx, versionSCTL)
+	install.FillLiensParcelleFermier(ctx, versionSCTL)
 	install.FillLiensParcelleUG(ctx)
 	fmt.Println("Migration effectuée : 2023-01-16-fix-parcelle")
-//	migrate_2023_01_16_refill_parcelle_ug(ctx)
 }
-
-// Fonctions auxiliaires, commencent par une miniscule
-
-func migrate_2023_01_16_refill_parcelle_ug(ctx *ctxt.Context) {
-    
-}
-
-/* 
-select * from chaufer_parcelle
- id_chaufer | id_parcelle | entiere | surface 
-------------+-------------+---------+---------
-          1 |        1577 | t       |       0
-          2 |        1025 | f       |       2
-          3 |        1262 | f       |       2
-          4 |        1262 | f       |       2
-          5 |        1319 | f       |       1
-          6 |        1253 | f       |       1
-          7 |        2203 | t       |       0
-
-*/
-
