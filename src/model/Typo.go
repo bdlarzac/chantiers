3b855a2b30c1523b72c2276dc40e0930285190dd
Thierry Graff <tigraff@gmail.com>
2023-03-03 18:53:09 +0100

    #15 - Prépare typo

---
+++ src/model/Typo.go
@@ -0,0 +1,21 @@
+/*
+*****************************************************************************
+
+	Typo = couche typologique
+
+	@copyright  BDL, Bois du Larzac.
+	@licence    GPL, conformémént au fichier LICENCE situé à la racine du projet.
+	@history    2023-03-03 12:11:29+01:00, Thierry Graff : Creation
+
+*******************************************************************************
+*/
+package model
+
+import (
+//"fmt"
+)
+
+type Typo struct {
+	Code     string
+	Nom      string
+}
