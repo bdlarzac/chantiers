cbd50b0e8beb4d3f72ba25e3aeeef18eaca4fe1d
Thierry Graff <tigraff@gmail.com>
2023-04-11 09:35:54 +0200

    #17 - début MVC pour affichage résultats de la recherche

--- src/control/search.go
+++ src/control/search.go
@@ -7,52 +7,81 @@ package control
 import (
 	"bdl.local/bdl/ctxt"
 	"bdl.local/bdl/model"
-	"net/http"
 	"time"
+	"net/http"
+"fmt"	
 )
 
-type detailsSearch struct {
+type detailsSearchForm struct {
 	Periods     [][2]time.Time    // pour choix-date
 	EssencesMap map[string]string // pour choix-essence
 	PropriosMap map[int]string    // pour choix-proprio
 	UrlAction   string
 }
 
+type detailsSearchResults struct{
+}
+
 /*
     Affiche / process le formulaire de recherche
 */
 func Search(ctx *ctxt.Context, w http.ResponseWriter, r *http.Request) (err error) {
-	ctx.TemplateName = "search.html"
-
-	//periods, hasChantier, err := model.ComputeLimitesSaisons(ctx.DB, ctx.Config.DebutSaison)
-	periods, _, err := model.ComputeLimitesSaisons(ctx.DB, ctx.Config.DebutSaison)
-	if err != nil {
-		return err
-	}
-	//
-	essencesMap, err := model.GetEssencesMap(ctx.DB)
-    if err != nil {
-        return err
+    
+	switch r.Method {
+	case "POST":
+		//
+		// Process form et affiche page de résultats
+		//
+        if err = r.ParseForm(); err != nil {
+            return err
+        }
+fmt.Println("ici")
+fmt.Printf("%+v\n",r.PostForm)
+        ctx.TemplateName = "search-result.html"
+        ctx.Page = &ctxt.Page{
+            Header: ctxt.Header{
+                Title: "Activités",
+            },
+            Menu: "accueil",
+            Details: detailsSearchResults{
+            },
+        }
+        return nil
+	default:
+		//
+		// Affiche form
+		//
+        //periods, hasChantier, err := model.ComputeLimitesSaisons(ctx.DB, ctx.Config.DebutSaison)
+        periods, _, err := model.ComputeLimitesSaisons(ctx.DB, ctx.Config.DebutSaison)
+        if err != nil {
+            return err
+        }
+        //
+        essencesMap, err := model.GetEssencesMap(ctx.DB)
+        if err != nil {
+            return err
+        }
+        //
+        propriosMap, err := model.GetProprietaires(ctx.DB)
+        if err != nil {
+            return err
+        }
+        //
+        ctx.TemplateName = "search-form.html"
+        ctx.Page = &ctxt.Page{
+            Header: ctxt.Header{
+                Title: "Recherche d'activité",
+                CSSFiles: []string{
+                    "/static/css/form.css"},
+            },
+            Menu: "accueil",
+            Details: detailsSearchForm{
+                Periods:   periods,
+                EssencesMap: essencesMap,
+                PropriosMap: propriosMap,
+                UrlAction: "/search",
+            },
+        }
+        return nil
     }
-	//
-	propriosMap, err := model.GetProprietaires(ctx.DB)
-    if err != nil {
-        return err
-    }
-    //
-	ctx.Page = &ctxt.Page{
-		Header: ctxt.Header{
-			Title: "Recherche d'activité",
-			CSSFiles: []string{
-				"/static/css/form.css"},
-		},
-		Menu: "accueil",
-		Details: detailsSearch{
-			Periods:   periods,
-			EssencesMap: essencesMap,
-			PropriosMap: propriosMap,
-			UrlAction: "/search",
-		},
-	}
-	return nil
 }
